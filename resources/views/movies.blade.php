b6fadb7325d69d07c9aeecae996d91ff24713657
Marilisa Moio <moiomarilisa@gmail.com>
2024-05-21 16:52:59 +0200

    chore: change item access method

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -22,13 +22,13 @@
             @foreach ($movies as $movie)
             <div class="card" style="width: 18rem;">
                 <div class="card-header bg-success text-white">
-                    {{ $movie['title'] }}
+                    {{ $movie->title }}
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">{{ $movie['original_title'] }}</li>
-                    <li class="list-group-item">{{ $movie['nationality'] }}</li>
-                    <li class="list-group-item">{{ $movie['date'] }}</li>
-                    <li class="list-group-item">{{ $movie['vote'] }}</li>
+                    <li class="list-group-item">{{ $movie->original_title }}</li>
+                    <li class="list-group-item">{{ $movie->nationality }}</li>
+                    <li class="list-group-item">{{ $movie->date }}</li>
+                    <li class="list-group-item">{{ $movie->vote }}</li>
                 </ul>
             </div>
             @endforeach
